cb7a0d9c44245ac706bb8075b8161384453b37dd
Kenniel-Torres <kennieltorres@gmail.com>
2023-05-24 20:35:39 -0400

    Began implementing Spotify authentication.

--- app.py
+++ app.py
@@ -12,12 +12,27 @@ app.config['SESSION_TYPE'] = 'filesystem'
 app.config['SESSION_FILE_DIR'] = './.flask_session/'
 Session(app)
 
+SCOPE = 'user-read-private user-read-email user-library-read playlist-read-private playlist-read-collaborative user-top-read'
+
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    cache_handler = spotipy.cache_handler.FlaskSessionCacheHandler(session)
+    auth_manager = spotipy.oauth2.SpotifyOAuth(scope=SCOPE,
+                                               cache_handler=cache_handler,
+                                               show_dialog=True)
+    auth_url = auth_manager.get_authorize_url()
+    return render_template('index.html', auth_url=auth_url)
 
-
+@app.route('/sign_in')
+def signin():
+    cache_handler = spotipy.cache_handler.FlaskSessionCacheHandler(session)
+    auth_manager = spotipy.oauth2.SpotifyOAuth(scope=SCOPE,
+                                               cache_handler=cache_handler,
+                                               show_dialog=True)
+    auth_url = auth_manager.get_authorize_url()
+    return redirect(auth_url)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
