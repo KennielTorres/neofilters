6578b3d6134e0f52b854e0ed2c2e11598aa385d3
Kenniel-Torres <kennieltorres@gmail.com>
2023-06-01 20:09:53 -0400

    Menu now partially displaying, and filtering options elements added.

--- static/js/playlist.js
+++ static/js/playlist.js
@@ -1,16 +1,20 @@
 
 window.onload = function() {
     let menu_toggle = document.getElementById('filter-toggle');
-    let filter_options_block = document.getElementById('filtering_options')
+    let filter_options_block = document.getElementById('filtering_options');
+    let menu_arrow = document.getElementById('toggle-arrow-img'); 
     
     menu_toggle.onclick = function() {
         if (this.checked){
-            console.log('menu toggle ON')
             filter_options_block.style.display = 'block'
+            menu_arrow.classList.remove('turn-up')
+            menu_arrow.classList.add('turn-down')
+
         }
         else {
-            console.log('menu toggle OFF')
             filter_options_block.style.display = 'none'
+            menu_arrow.classList.remove('turn-down')
+            menu_arrow.classList.add('turn-up')
         }
     };
 };
