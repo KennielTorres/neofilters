3eda43e8f53c304de1826415a0b217e1bf784da2
Kenniel-Torres <kennieltorres@gmail.com>
2023-06-07 19:38:44 -0400

    More backend data handling and disclaimer in landing page.

--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 
 
 '''
@@ -13,6 +14,10 @@ def process_data(items):
         seconds = (duration_ms % 60000) // 1000
         item['track']['duration_ms'] = f"{minutes}:{seconds:02d}"
 
+        # If no release date provided, default to client's current date
+        if (item['track']['album']['release_date'] == None):
+            item['track']['album']['release_date'] = datetime.now().strftime('%Y-%m-%d')
+
         # Release date has no month & day. Default to {release_year}-01-01
         if (len(item['track']['album']['release_date']) == 4 and len(item['track']['album']['release_date']) < 7):
             item['track']['album']['release_date'] = f"{item['track']['album']['release_date']}-01-01"
@@ -21,8 +26,12 @@ def process_data(items):
         if (len(item['track']['album']['release_date']) == 7 and len(item['track']['album']['release_date']) < 10):
             item['track']['album']['release_date'] = f"{item['track']['album']['release_date']}-01"
 
-        # Forces 'isrc' to uppercase
-        item['track']['external_ids']['isrc'] = (item['track']['external_ids']['isrc']).upper()
+        # Check if item has 'isrc' else it adds a placeholder
+        if ('isrc' not in item['track']['external_ids']):
+            item['track']['external_ids']['isrc'] = 'NOVAL0123456'
+        elif ('isrc' in item['track']['external_ids']):
+            # Forces 'isrc' to uppercase
+            item['track']['external_ids']['isrc'] = (item['track']['external_ids']['isrc']).upper()
 
         # Removes time from 'added_at'
         item['added_at'] = item['added_at'][:10]
